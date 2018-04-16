d4d552bba94621f73dccf1e14bc1e46138e12127
Maxim Chepel <text.thunder@gmail.com>
2018-04-16 14:33:13 +0300

    npm.init(project(webpack)).react

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const indexHTML = require('./webpack/index.html.config.js');
+
+module.exports = {
+    entry: [
+        path.join(__dirname, 'src', 'metasphere.application.jsx')
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(jsx|js)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react']
+                    }
+                }
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /node_modules/,
+                    chunks: 'all',
+                    name: 'vendor',
+                    priority: 10,
+                    enforce: true
+                }
+            }
+        }
+    },
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: '[name].js'
+    },
+    plugins: [
+        indexHTML.plugin()
+    ],
+    devServer: {
+        host: 'localhost',
+        port: 9090,
+        open: false,
+        allowedHosts: [
+            'localhost'
+        ]
+    },
+};
---
+++ webpack/index.html.config.js
@@ -0,0 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    plugin: () => {
+        return new HtmlWebpackPlugin({
+            title: 'Metasphere ECMAScript2017 Prototype',
+            template: './src/metasphere.template.html'
+        })
+    }
+};
